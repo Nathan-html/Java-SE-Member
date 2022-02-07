edf35278568f6adcdf8eb22205db49ac2da9bba0
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-02-07 10:17:22 +0100

    Creating Member class and the first member

--- src/com/nathan_html/member/Main.java
+++ src/com/nathan_html/member/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Member steve = new Member("steven", "zbliat");
+		System.out.println(steve.getLastName());
 	}
 
 }
